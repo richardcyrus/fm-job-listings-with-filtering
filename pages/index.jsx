19243c8e28e618c3f2d6e577b52f31ad2cb049b0
Richard Cyrus <richard.cyrus@rcyrus.com>
2021-07-03 18:43:51 -0400

    Add the display of the job cards

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,7 +1,22 @@
 import Layout from '../components/Layout'
+import Card from '../components/Card'
+
+import { jobList } from '../data/data'
+
+export const getStaticProps = async () => {
+  return {
+    props: {
+      jobs: jobList,
+    },
+  }
+}
+
 export default function Home({ jobs }) {
   return (
     <Layout>
+      {jobs.map((job) => (
+        <Card key={job.id} {...job} />
+      ))}
     </Layout>
   )
 }
