41c1aab7a9510aa1129ae479bf58c8bf6a0eb532
Richard Cyrus <richard.cyrus@rcyrus.com>
2021-07-03 22:19:36 -0400

    Fix wrong tag name

--- components/Heading/index.jsx
+++ components/Heading/index.jsx
@@ -1,5 +1,5 @@
 import styles from './heading.module.scss'
 
 export default function Heading() {
-  return <heading className={styles.siteHeader} />
+  return <header className={styles.siteHeader} />
 }
